b1b7ea3b3aaa3f5554b10e379789956ac7208b7a
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2020-12-02 15:50:50 +0200

    Hoist rest of typeDefs into location and reservation specific schemata

--- schemata/location.js
+++ schemata/location.js
@@ -1,1 +1,58 @@
-exports.locationSchema = "";
+exports.locationSchema = `
+
+  """
+  A place that forms a unit and can be used for some specific purpose -
+  respa unit or resource, service map unit, beta.kultus venue, linked
+  events place, Kukkuu venue
+  """
+  type Venue {
+    meta: NodeMeta
+    name: String
+    location: LocationDescription
+    description: String
+    descriptionResources: DescriptionResources
+    partOf: Venue
+    openingHours: OpeningHours
+    manager: LegalEntity
+    contactDetails: ContactInfo
+    reservationPolicy: VenueReservationPolicy
+    accessibilityProfile: AccessibilityProfile
+    arrivalInstructions: String
+    additionalInfo: String
+    facilities: [VenueFacility!]
+  }
+
+  """
+  Free-form location, not necessarily at a know venue.
+  """
+  type LocationDescription {
+    url: String
+    geoLocation: GeographicalLocation
+    streetAddress: Address
+    explanation: String
+    venue: Venue
+  }
+
+  """TODO: take this from service map / TPREK"""
+  type AccessibilityProfile {
+    meta: NodeMeta
+    todo: String
+  }
+
+  """TODO: combine beta.kultus Venue stuff with respa equipment type"""
+  type VenueFacility {
+    meta: NodeMeta
+    name: String!
+    categories: [Keyword!]
+  }
+
+  """
+  This should be just any GeoJSON object, see
+  https://github.com/ghengeveld/graphql-geojson
+  """
+  type GeographicalLocation {
+    type: String!
+    coordinates: [Float!]!
+  }
+
+`;
--- schemata/reservation.js
+++ schemata/reservation.js
@@ -1,1 +1,14 @@
-exports.reservationSchema = "";
+exports.reservationSchema = `
+
+  """TODO: take this from respa / Hauki"""
+  type OpeningHours {
+    meta: NodeMeta
+    todo: String
+  }
+
+  """TODO: this comes from respa resource/unit types"""
+  type VenueReservationPolicy {
+    todo: String
+  }
+
+`;
