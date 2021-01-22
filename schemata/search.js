b3f237b191d8eb736094e295fcd48f35b4d494e7
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2021-01-22 13:56:58 +0200

    Some example data for unified search

--- schemata/search.js
+++ schemata/search.js
@@ -36,12 +36,45 @@ exports.searchSchema = `
     description: LanguageString!
     resources: DescriptionResources
     canonicalUrl: String!
+    searchCategories: [UnifiedSearchResultCategory!]!
   }
 
 `;
 
 exports.searchExample = {
-
+  "count": 1,
+  "max_score": 0.486115,
+  "pageInfo": {
+    "hasNextPage": false,
+    "hasPreviousPage": false,
+    "startCursor": "iofj934ji43jfi3j4fire",
+    "endCursor": null,
+  },
+  edges: [
+    {
+      "cursor": "iofj934ji43jfi3j4fire",
+      "node": {
+	"_score": 0.486115,
+	"id": "lippupiste:serie-2804177",
+	"name": {
+	  "fi": "Vain Parasta Minulle",
+	  "text": "Vain Parasta Minulle",
+	  "defaultLanguage": "FI",
+	},
+	"description": {
+	  "fi": "<p><strong>Helsingin Kaupunginteatterin pienellä näyttämöllä esitettävät ...</strong></p>",
+	  "text": "<p><strong>Helsingin Kaupunginteatterin pienellä näyttämöllä esitettävät ...</strong></p>",
+	  "defaultLanguage": "FI",
+	},
+	"resources": {
+	  "infoUrls": ["https://www.lippu.fi/tickets.html?affiliate=adv&doc=erdetaila&fun=erdetail&erid=2856915"],
+	  "mediaResources": [],
+	},
+	"canonicalUrl": "https://tapahtumat.hel.fi/fi/event/helsinki:af2tbff5zm",
+	"searchCategories": ["EVENT"],
+      },
+    },
+  ],
 };
 
 // vi:set sw=2:
